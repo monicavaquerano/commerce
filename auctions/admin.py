d6845dbb6d2b5f2cd43173e7d143582f61d82ae2
monicavaquerano <106926593+monicavaquerano@users.noreply.github.com>
2023-06-10 17:40:19 +0200

    update admin

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,13 +1,39 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Listings, Comments, Bids, Category
 
 
 # Register your models here.
 class UserAdmin(admin.ModelAdmin):
-    list_display = ("id", "username", "email", "password")
+    list_display = (
+        "id",
+        "username",
+        "email",
+        "password",
+    )
+
+
+class ListingsAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "description",
+        "image",
+        "category",
+        "start_bid",
+        "user",
+        "start_date",
+        "end_date",
+        "duration",
+        "is_active",
+    )
+    filter_horizontal = ("category", "watchers")
 
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Listings, ListingsAdmin)
+admin.site.register(Category)
+admin.site.register(Bids)
+admin.site.register(Comments)
 
 
 # from django.contrib import admin
