913dfafa8549ec820df116194ab7b4c13e8d618f
monicavaquerano <106926593+monicavaquerano@users.noreply.github.com>
2023-08-09 14:10:14 +0200

    changes admin

--- auctions/admin.py
+++ auctions/admin.py
@@ -3,19 +3,44 @@ from .models import User, Listings, Comments, Bids, Category
 
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["id", "username", "email", "first_name", "last_name"]
+
+
 class ListingsAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "title",
+        "description",
+        "start_bid",
+        "start_date",
+        "user",
+        "is_active",
+    ]
+    list_filter = [
+        ("is_active", admin.BooleanFieldListFilter),
+    ]
+
     filter_horizontal = ("watchers",)
 
 
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ["id", "listing", "user", "comment", "datetime"]
+    ordering = ["datetime", "listing"]
+    list_filter = ["listing", "user"]
+
+
 class BidsAdmin(admin.ModelAdmin):
-    ...
+    list_display = ["id", "listing", "amount", "user", "datetime"]
+    ordering = ["listing"]
+    list_filter = ["listing"]
 
 
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Listings, ListingsAdmin)
 admin.site.register(Category)
 admin.site.register(Bids, BidsAdmin)
-admin.site.register(Comments)
+admin.site.register(Comments, CommentsAdmin)
 
 
 # from django.contrib import admin
